a6779fd7648df1e56328092911d3e0e1082456db
rubens23 <rubens_assis@outlook.com.br>
2023-10-12 10:49:31 -0300

    init commit

--- app/src/main/java/com/rubens/monthselectorlibrary/MainActivity.kt
+++ app/src/main/java/com/rubens/monthselectorlibrary/MainActivity.kt
@@ -2,15 +2,13 @@ package com.rubens.monthselectorlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.rubens.month_selector.MonthSelectorHelper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val ms = MonthSelectorHelper()
-        ms.addOneMonth()
+
 
     }
 }
